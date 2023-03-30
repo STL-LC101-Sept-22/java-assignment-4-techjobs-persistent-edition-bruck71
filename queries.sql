20c210d4ea8292175e743b21a62cd96afba2ff45
Brad Rucker <bruck63@gmail.com>
2023-03-30 17:39:38 -0500

    Fixed my query to pass the last test in TestTaskFour

--- queries.sql
+++ queries.sql
@@ -16,7 +16,7 @@ DROP TABLE job;
 
 SELECT *
 FROM skill
-INNER JOIN job_skills ON (job_skills.skills_id = skill.id)
+INNER JOIN job_skills ON job_skills.skills_id = skill.id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
 
